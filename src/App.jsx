3ce06f5f4cb59bef1163f29550433311f3291fba
ali96office-gif <ali96.office@gmail.com>
2026-06-26 23:13:44 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 // ══════════════════════════════════════════════════════════════
 //  ربط Google Sheets
 // ══════════════════════════════════════════════════════════════
-const GS_URL = "https://script.google.com/macros/s/AKfycbylkHKK64ITAv49uej6YiKKPDhuuuxOWxG-7zqCCfjV10p2FD1gk91H_Yh7iiU2Z4tI/exec";
+const GS_URL = "https://script.google.com/macros/s/AKfycbwGTO7S5M04p6c_SQNUOo-CH9pfb4FnFcE3iEDCzENR-pK1nmnBO0y4b5gbPLe2oSG0/exec";
 
 async function gsSaveAttendance(emp, record) {
   try {
